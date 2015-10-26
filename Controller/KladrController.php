41e443d56d30140ae720935a5e537958d5a47ef2
Ilya Gusev <mail@igusev.ru>
2015-10-26 16:34:21 +0300

    Update KladrController.php
    Убрал ограничения по поиску только в определенных регионах

--- Controller/KladrController.php
+++ Controller/KladrController.php
@@ -22,7 +22,6 @@ class KladrController extends Controller
         $entities = $em->getRepository("KladrBundle:Kladr")->createQueryBuilder('o')
             ->where('o.name LIKE :name')
             ->andWhere("o.code LIKE '%00'")
-            ->andWhere("o.code in ('2300000000000', '6100000000000', '3400000000000', '3000000000000')")
             ->andWhere("o.socr in ('Респ','Чувашия','край','обл','Аобл','АО')")
             ->setParameter('name', "%$region%")
             ->getQuery()
